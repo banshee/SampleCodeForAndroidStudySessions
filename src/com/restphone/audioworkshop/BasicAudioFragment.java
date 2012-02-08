a6330c6688a1337103705b4c973f7a40fc16ff2d
James Moore <james@restphone.com>
2012-02-08 12:26:52 -0800

    Hooked up player controls

--- src/com/restphone/audioworkshop/BasicAudioFragment.java
+++ src/com/restphone/audioworkshop/BasicAudioFragment.java
@@ -4,8 +4,12 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
+import android.widget.MediaController;
+import android.widget.MediaController.MediaPlayerControl;
 
 public class BasicAudioFragment extends Fragment {
   @Override
@@ -16,12 +20,83 @@ public class BasicAudioFragment extends Fragment {
     // Inflate the layout for this fragment
     return inflater.inflate(R.layout.basic_audio_player, container, false);
   }
-  
+
   @Override
   public void onResume() {
     super.onResume();
 
-    MediaPlayer mediaPlayer = MediaPlayer.create(getActivity(), R.raw.testmp3);
+    final MediaPlayer mediaPlayer = MediaPlayer.create(getActivity(),
+        R.raw.testmp3);
     mediaPlayer.start(); // no need to call prepare(); create() does that for
+
+    final MediaController mediaController = new MediaController(getActivity());
+    View audioView = getActivity().findViewById(R.id.imageView1);
+    mediaController.setAnchorView(audioView);
+
+    // Notice that we are still not hooked up yet
+
+    MediaPlayerControl x = new MediaController.MediaPlayerControl() {
+      @Override
+      public void start() {
+        mediaPlayer.start();
+      }
+
+      @Override
+      public void pause() {
+        mediaPlayer.pause();
+      }
+
+      @Override
+      public int getDuration() {
+        return mediaPlayer.getDuration();
+      }
+
+      @Override
+      public int getCurrentPosition() {
+        return mediaPlayer.getCurrentPosition();
+      }
+
+      @Override
+      public void seekTo(int pos) {
+        mediaPlayer.seekTo(pos);
+      }
+
+      @Override
+      public boolean isPlaying() {
+        return mediaPlayer.isPlaying();
+      }
+
+      @Override
+      public int getBufferPercentage() {
+        float currentPosition = mediaPlayer.getCurrentPosition();
+        float bufferLength = mediaPlayer.getDuration();
+        return (int) ((currentPosition / bufferLength) * 1000);
+      }
+
+      @Override
+      public boolean canPause() {
+        return true;
+      }
+
+      @Override
+      public boolean canSeekBackward() {
+        return true;
+      }
+
+      @Override
+      public boolean canSeekForward() {
+        return true;
+      }
+    };
+
+    mediaController.setMediaPlayer(x);
+
+    audioView.setOnTouchListener(new OnTouchListener() {
+      @Override
+      public boolean onTouch(View v, MotionEvent event) {
+        mediaController.show();
+        return true;
+      }
+    });
   }
 }
