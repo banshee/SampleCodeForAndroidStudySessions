8f77de8f24bfa839bf90adcc379913db2cf15d50
James Moore <james@restphone.com>
2012-02-08 11:36:16 -0800

    Play audio file

--- src/com/restphone/audioworkshop/AudioWorkshopActivity.java
+++ src/com/restphone/audioworkshop/AudioWorkshopActivity.java
@@ -1,13 +1,17 @@
 package com.restphone.audioworkshop;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class AudioWorkshopActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+
+     MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.testmp3);
+     mediaPlayer.start(); // no need to call prepare(); create() does that for
+  }
 }
